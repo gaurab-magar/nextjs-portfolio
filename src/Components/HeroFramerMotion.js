207028ca7162ddce72de2bb04f0057b2bd6ceaae
Your Name <gdikshan10@gmail.com>
2024-06-20 02:41:52 +0545

    updated escape character fpr deploying

--- src/Components/HeroFramerMotion.js
+++ src/Components/HeroFramerMotion.js
@@ -12,14 +12,14 @@ const HeroFramerMotion = () => {
         animate={{ x: 0, opacity: 1 }}
         transition={{ duration: 1,delay: 0.5 }}    
         >
-            <h1 className="text-2xl md:text-4xl font-bold mb-4 font-mono  ">Hi,I am Gaurab Magar</h1>
+            <h1 className="text-2xl md:text-4xl font-bold mb-4 font-mono  ">Hi I am Gaurab Magar</h1>
         </motion.div>
         <motion.div
         initial={{ x: 40, opacity: 0 }}
         animate={{ x: 0, opacity: 1 }}
         transition={{ duration: 1, delay: 0.5 }}
         >
-            <p className="text-xl md:text-2xl font-extralight md:w-8/12 mx-auto"> I'm a passionate frontend developer with a keen eye for design.specialized in creating beautiful, responsive, and user-friendly web applications.</p>
+            <p className="text-xl md:text-2xl font-extralight md:w-8/12 mx-auto"> I m a passionate frontend developer with a keen eye for design.specialized in creating beautiful, responsive, and user-friendly web applications.</p>
         </motion.div>
         <motion.div
         initial={{ y: 40, opacity: 0 }}
--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,14 @@
 import HeroSection from "./HeroSection";
-import Featured from "./Featured";
-import About from "./About";
-import Contact from "./Contact";
+
 // import Skills from "./Skills";
 
 export default function Home() {
   return (
     <div>
         <HeroSection />
-        <Featured />
+        {/* <Featured />
         <About />
-        <Contact />
+        <Contact /> */}
     </div>
   );
 }
